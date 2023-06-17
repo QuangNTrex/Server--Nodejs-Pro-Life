f06aa15cf3fa23dc361b1e543f2af23e1d49a316
Tien Quang <quangnt.rex@gmail.com>
2023-06-17 20:07:35 +0700

    jsadasdasd

--- routers/auth.js
+++ routers/auth.js
@@ -3,7 +3,7 @@ const AuthController = require("../controllers/auth");
 const isAuth = require("../middleware/is-auth").isAuth;
 
 router.post("/signin", AuthController.postSignIn);
-router.post("/signup", isAuth, AuthController.postSignUp);
+router.post("/signup", AuthController.postSignUp);
 router.post("/signout", isAuth, AuthController.postSignOut);
 
 module.exports = router;
